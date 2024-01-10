c9174260883c1abf9c8e4f0097e41fa24bf7e38e
Sumeet Mishra <116353839+sumeetmishra809@users.noreply.github.com>
2024-01-10 18:32:41 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,2 @@
- 
-
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-
-export const auth = firebase.initializeApp({
-    apiKey: "AIzaSyBNQp50RwYjyaox6lBr_A49a9X_VajMr4I",
-    authDomain: "unichat-32c5a.firebaseapp.com",
-    projectId: "unichat-32c5a",
-    storageBucket: "unichat-32c5a.appspot.com",
-    messagingSenderId: "245537172031",
-    appId: "1:245537172031:web:b0d884e39fe025d24c268a",
-    measurementId: "G-PQSB67D9TW"
-}).auth();
-
+////////////////////////////////
+/////////////////////////////
